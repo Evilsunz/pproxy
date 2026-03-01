25cf3d65dc07cfcf10c29361e64a9814f2bdb561
Maxim <maxim_dmytrenko@comcast.com>
2026-03-01 19:47:06 +0200

    Commit XXXIX

--- src/oauth2.rs
+++ src/oauth2.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+mod tests;
+
 use crate::config::RPConfig;
 use crate::lb::{AuthClaims, AuthDecision, AuthVerifier};
 use crate::{log_error, log_trace};
@@ -253,71 +256,5 @@ impl AuthVerifier {
             .url();
 
         Ok(auth_url.to_string())
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    fn mock_verifier() -> AuthVerifier {
-        AuthVerifier::new_for_tests(RPConfig::default())
-    }
-
-    #[test]
-    fn decide_auth_redirects_when_no_cookie_header() {
-        let v = mock_verifier();
-        let uri: Uri = "http://example.local/".parse().unwrap();
-
-        let d = v.decide_auth(&uri, None);
-        assert_eq!(d, AuthDecision::RedirectToSso);
-    }
-
-    #[test]
-    fn decide_auth_redirects_when_cookie_missing() {
-        let v = mock_verifier();
-        let uri: Uri = "http://example.local/".parse().unwrap();
-
-        let d = v.decide_auth(&uri, Some("other=1; something=2"));
-        assert_eq!(d, AuthDecision::RedirectToSso);
-    }
-
-    #[test]
-    fn decide_auth_redirects_when_code_query_param_is_present() {
-        let v = mock_verifier();
-        let uri: Uri = "http://example.local/?code=abababbsdkajsdlkasl"
-            .parse()
-            .unwrap();
-
-        let d = v.decide_auth(&uri, None);
-        assert_eq!(
-            d,
-            AuthDecision::Exchange {
-                code: "abababbsdkajsdlkasl".to_string()
-            }
-        );
-    }
-
-    #[test]
-    fn decide_auth_proceeds_when_cookie_present() {
-        let v = mock_verifier();
-        let uri: Uri = "http://example.local/".parse().unwrap();
-
-        let jwt = v.encode_jwt("xxx", "yyy").unwrap();
-
-        let d = v.decide_auth(&uri, Some(&format!("rproxy_auth={}; other=1", jwt)));
-        assert_eq!(d, AuthDecision::Proceed);
-    }
-
-    #[test]
-    fn decide_auth_proceeds_when_cookie_present_jwt_malformed() {
-        let v = mock_verifier();
-        let uri: Uri = "http://example.local/".parse().unwrap();
-
-        let d = v.decide_auth(
-            &uri,
-            Some(&format!("rproxy_auth=asdasdasdasdasdasd; other=1")),
-        );
-        assert_eq!(d, AuthDecision::RedirectToSso);
     }
 }
