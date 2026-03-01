27da397610ddc8778380ba405319512b963c4790
Maxim <maxim_dmytrenko@comcast.com>
2026-03-01 18:58:14 +0200

    Commit XXXVII

--- src/oauth2.rs
+++ src/oauth2.rs
@@ -229,6 +229,15 @@ mod tests {
     }
 
     #[test]
+    fn decide_auth_redirects_when_code_query_param_is_present() {
+        let v = mock_verifier();
+        let uri: Uri = "http://example.local/?code=abababbsdkajsdlkasl".parse().unwrap();
+
+        let d = v.decide_auth(&uri, None);
+        assert_eq!(d, AuthDecision::Exchange { code: "abababbsdkajsdlkasl".to_string() });
+    }
+
+    #[test]
     fn decide_auth_proceeds_when_cookie_present() {
         let v = mock_verifier();
         let uri: Uri = "http://example.local/".parse().unwrap();
@@ -238,4 +247,14 @@ mod tests {
         let d = v.decide_auth(&uri, Some(&format!("rproxy_auth={}; other=1", jwt)));
         assert_eq!(d, AuthDecision::Proceed);
     }
+
+    #[test]
+    fn decide_auth_proceeds_when_cookie_present_jwt_malformed() {
+        let v = mock_verifier();
+        let uri: Uri = "http://example.local/".parse().unwrap();
+
+        let d = v.decide_auth(&uri, Some(&format!("rproxy_auth=asdasdasdasdasdasd; other=1")));
+        assert_eq!(d, AuthDecision::RedirectToSso);
+    }
+
 }
